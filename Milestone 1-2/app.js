b47a14599cd05ee509c8da98b7bf3c4624388910
Kashaf Naz <kashafnazk389@gmail.com>
2024-11-09 23:03:53 +0500

    static resume builder

--- Milestone 1-2/app.js
+++ Milestone 1-2/app.js
@@ -1,10 +1,23 @@
-const moreInfo = document.getElementById("moreInfo") ;
-const button = document.getElementById("moreInfoBtn"); 
-
-function buttonrun() {
-    // Check if the moreInfo section is hidden
-    if (moreInfo.style.display === "none" || moreInfo.style.display === "") {
-        moreInfo.style.display = "block";  // Show the hidden info
-        button.style.display = "none";     // Hide the button
+//  const button = document.getElementById("moreInfoBtn") as HTMLButtonElement;
+//  const moreInfo = document.getElementById("moreInfo") as HTMLElement;
+//  function buttonrun() {
+//      if (moreInfo.style.display === "none") {
+//          moreInfo.style.display = "block";  
+//          button.style.display = "none";  
+//     }
+//  }
+// Function to toggle visibility of information
+function showhidebtn() {
+    var moreInformation = document.getElementById("moreInfo");
+    var moreInfoBtn = document.getElementById("moreInfoBtn");
+    if (moreInfoBtn && moreInformation) {
+        if (moreInformation.classList.contains("hidden")) {
+            moreInformation.classList.remove("hidden");
+            moreInfoBtn.style.display = "none";
+        }
+        else {
+            moreInformation.classList.add("hidden");
+            moreInfoBtn.style.display = "inline-block";
+        }
     }
 }
--- Milestone 1-2/app.ts
+++ Milestone 1-2/app.ts
@@ -1,10 +1,25 @@
 
- const button = document.getElementById("moreInfoBtn") as HTMLButtonElement;
- const moreInfo = document.getElementById("moreInfo") as HTMLElement;
+//  const button = document.getElementById("moreInfoBtn") as HTMLButtonElement;
+//  const moreInfo = document.getElementById("moreInfo") as HTMLElement;
 
- function buttonrun() {
-     if (moreInfo.style.display === "none") {
-         moreInfo.style.display = "block";  
-         button.style.display = "none";  
+//  function buttonrun() {
+//      if (moreInfo.style.display === "none") {
+//          moreInfo.style.display = "block";  
+//          button.style.display = "none";  
+//     }
+//  }
+// Function to toggle visibility of information
+function showhidebtn(){
+    const moreInformation = document.getElementById("moreInfo");
+    const moreInfoBtn = document.getElementById("moreInfoBtn");
+    if(moreInfoBtn && moreInformation){
+        if (moreInformation.classList.contains("hidden")) {
+            moreInformation.classList.remove("hidden");
+            moreInfoBtn.style.display = "none";
+        } else {
+            moreInformation.classList.add("hidden");
+            moreInfoBtn.style.display = "inline-block";
+        }
     }
- }
+
+}
